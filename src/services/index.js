c1cc5501fc28e8ee7c72593be83d5d66ceb33850
joao victor santos rodrigues <imashinzo10@gmail.com>
2023-03-12 23:40:26 -0300

    Push corrigido

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
-const UserService = require('./userService');
-const CategoryService = require('./categoryService');
-const PostService = require('./postService');
+const UserService = require('./user.service');
+const CategoryService = require('./category.service');
+const PostService = require('./post.service');
 
 module.exports = {
   UserService,
